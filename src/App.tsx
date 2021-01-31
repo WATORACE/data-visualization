395f5c8d71c15d1314719e99527c2d87df6c96e8
Ben Zhang <benzhangniu@gmail.com>
2021-01-31 12:32:42 -0800

    Add new graphs and synchronization, and add the ability to apply/export configurations

--- src/App.tsx
+++ src/App.tsx
@@ -4,61 +4,123 @@ import './App.css';
 import Papa, {ParseResult as PapaParseResult} from 'papaparse';
 import uPlot from 'uplot';
 import 'uplot/dist/uPlot.min.css';
+import {deepCopy} from './utils';
+import _ from 'lodash';
 
 function App() {
   const fileInput = useRef(null as (HTMLInputElement|null));
+  const rawConfig = useRef(null as (HTMLTextAreaElement|null));
   const [errorMsgs, setErrorMsgs] = useState([] as Array<string>);
   const [datasets, setDatasets] = useState([] as Array<{file: File | undefined, parsed: PapaParseResult<unknown>}>);
+  const [visualizations, setVisualizations] = useState([
+    {
+      title: "Steering",
+      height: 300,
+      cursor: {
+        sync: {
+          key: "moo",
+        },
+      },
+      inputs: [
+        {
+          label: "time (s)",
+          data: "0.TimeOfUpdate",
+        },
+        {
+          label: "Steering Wheel Angle (rad)",
+          stroke: "red",
+          width: 1,
+          data: "0.SteeringWheelAngle",
+        },
+        // {
+        //   label: "Steering Wheel Angle (deg)",
+        //   stroke: "blue",
+        //   width: 1,
+        // },
+      ]
+    },
+    {
+      title: "Speed (in vehicle frame)",
+      height: 300,
+      cursor: {
+        sync: {
+          key: "moo",
+        },
+      },
+      inputs: [
+        {
+          label: "time (s)",
+          data: "0.TimeOfUpdate",
+        },
+        {
+          label: "cdgSpeed_x",
+          stroke: "blue",
+          width: 1,
+          data: "0.cdgSpeed_x",
+        },
+        {
+          label: "cdgSpeed_y",
+          stroke: "green",
+          width: 1,
+          data: "0.cdgSpeed_y",
+        }
+      ]
+    }
+  ]);
 
   // plot datasets on change (rerender of the react component)
   useEffect(() => {
-    if (datasets.length === 0) {
+    if (visualizations.length === 0) {
       return;
     }
-    const idx = 0
-    const dataset = datasets[idx];
-    const data = [];
-    data[0] = dataset.parsed.data.map(row => +(row as any).TimeOfUpdate);
-    data[1] = dataset.parsed.data.map(row => +(row as any).SteeringWheelAngle);
 
-    const opts = {
-					title: "Steering over time (rad)",
-					width: 1920,
-          height: 600,
-          scales: {
-            x: {
-              time: false
-            }
-          },
-					series: [
-						{
-              label: "time (s)"
-            },
-						{
-							label: "Steering Wheel Angle (rad)",
-							value: (self: any, rawValue: any) => rawValue.toFixed(2),
-							stroke: "red",
-							width: 1/devicePixelRatio,
-						},
-					],
-					axes: [
-						{},
-						{
-							values: (u: any, vals: Array<number>, space: any) => vals.map(v => +v.toFixed(2)),
-						},
-					],
-        };
-        
-        const plotContainer = document.getElementById(`plot-${idx}`);
+    const plots = [] as Array<uPlot>;
 
-        if (!plotContainer) {
-          // TODO: Fix react error when using setErrorMsgs in effect
-          // setErrorMsgs(existingMsgs => [...existingMsgs, `Plot container not found for plot-${idx}`]);
-          return;
+    for (let i = 0; i < visualizations.length; ++i) {
+      const visualization = visualizations[i];
+      const {title, inputs, cursor, height} = visualization;
+      const data = []; // uPlot data
+      const series = []; // uPlot series
+      console.log("Processing visualization", i)
+      for (let j = 0; j < inputs.length; ++j) {
+        const {data: dataPathStr, width, ...otherInputs} = inputs[j];
+        const dataPath = dataPathStr.split(".");
+        // extract the data based on the input datapath
+        const dataset = datasets[+dataPath[0]];
+        if (!dataset) {
+          setErrorMsgs(existingMsgs => [...existingMsgs, `Unable to access ${dataPathStr} because dataset ${dataPath[0]} is not available!`]);
+          continue;
         }
+        data[j] = dataset.parsed.data.map(row => +(row as {[n: string]: number})[dataPath[1]]);
+        series[j] = {width: width ? width/devicePixelRatio : undefined, ...otherInputs};
+      };
+      const opts = {
+        title,
+        cursor,
+        width: window.innerWidth,
+        height,
+        scales: {
+          x: {
+            time: false
+          }
+        },
+        series
+      }
 
-				let uplot = new uPlot(opts, data, plotContainer);
-  });
+      const plotContainer = document.getElementById(`plot-${i}`);
+
+      if (!plotContainer) {
+        setErrorMsgs(existingMsgs => [...existingMsgs, `Plot container not found for plot-${i}`]);
+        return;
+      }
+
+      plots.push(new uPlot(opts, data, plotContainer));
+    }
+
+    return ()=>{
+      plots.forEach(plot => plot.destroy());
+    }
+  }, [datasets, visualizations]);
 
   function handleAddDataset() {
     setErrorMsgs([]);
@@ -91,20 +153,55 @@ function App() {
     }
   }
 
+  function handleApplyRawConfig() {
+    const rawConfigStr = rawConfig.current?.value;
+    if (!rawConfigStr) {
+      setErrorMsgs(existingMsgs => [...existingMsgs, "Invalid config!"]);
+      return;
+    }
+
+    try {
+      setVisualizations(JSON.parse(rawConfigStr).visualizations);
+    } catch (error) {
+      setErrorMsgs(existingMsgs => [...existingMsgs, "Error applying config. Please see console for more info"]);
+      console.error("Error applying config:", error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="Content">
+        <h1>WATORACE Data Visualizer</h1>
         { errorMsgs.map((msg, idx) => <p key={idx} className="error-message">{msg}</p>) }
         <p>{datasets.length} Dataset(s)</p>
-        <ol>
+        <ol start={0}>
           { datasets.map((dataset, idx) => <li key={idx}>{dataset.file?.name}</li>) }
         </ol>
         <input type="file" ref={fileInput} multiple />
         <button onClick={handleAddDataset}>Add Dataset</button>
+        { visualizations.map((visualization, idx) => (
+            <div key={idx}>
+              <p>{visualization.title}</p>
+              <div id={`plot-${idx}`}/>
+            </div>
+          ))
+        }
+
+        <h2>Raw Configuration</h2>
+        <p>This <code>textarea </code> contains the raw configuration that generates the graphs above. You can copy the text below and share with others. You can also apply new configurations by replacing the text below and clicking "Apply configuration".</p>
+        <textarea id="raw-config" ref={rawConfig}>{JSON.stringify({visualizations}, null, 2)}</textarea>
+        <br/>
+        <button onClick={handleApplyRawConfig}>Apply Configuration</button>
+      </div>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          WATORACE Data Visualizer
+        </p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -114,13 +211,6 @@ function App() {
           Learn React
         </a>
       </header>
-      { datasets.map((dataset, idx) => (
-          <div key={idx}>
-            <p>{dataset.file?.name}</p>
-            <div id={`plot-${idx}`}/>
-          </div>
-        ))
-      }
     </div>
   );
 }
